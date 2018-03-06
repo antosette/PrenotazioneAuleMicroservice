74a8ed6a613b3c40a57a532eb488c02c53b9d488
Antonio <antosette@gmail.com>
2018-03-06 18:29:21 +0100

    Initial commit

---
+++ src/main/java/com/marconivr/prenotazioneaule/PrenotazioneAuleMicroservice/PrenotazioneAuleMicroserviceApplication.java
@@ -0,0 +1,12 @@
+package com.marconivr.prenotazioneaule.PrenotazioneAuleMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrenotazioneAuleMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrenotazioneAuleMicroserviceApplication.class, args);
+	}
+}
